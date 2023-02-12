6f25d8f7a0de3b3d989e2dd3da3d2fd149718ae1
MyHappySun <112709363+DahakaDevelopDevOps@users.noreply.github.com>
2023-02-12 13:46:59 +0300

    created repository for my java-code listings

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import  javax.swing.JOptionPane;
+import  java.util.Scanner;
+public class Main {
+    public static void main(String[] args)
+    {
+//        String title = "Сообщение";
+//        String text = "Продолжаем изучать Java";
+//        JOptionPane.showMessageDialog(null, title,text,JOptionPane.WARNING_MESSAGE);
+//        System.out.println("Console output");
+//        text = JOptionPane.showInputDialog("Input text");
+//        JOptionPane.showMessageDialog(null, "Inputed text " + text);
+//        title = JOptionPane.showInputDialog("Enter title for window", JOptionPane.WARNING_MESSAGE);
+//        text = JOptionPane.showInputDialog("Enter text for window", JOptionPane.PLAIN_MESSAGE);
+//        JOptionPane.showMessageDialog(null, text, title, JOptionPane.INFORMATION_MESSAGE);
+        //creation of scanner object
+//        Scanner input = new Scanner(System.in);
+//        //variables for month  nad day scanning
+//        String day, month;
+//        //Display of the message
+//        System.out.println("Which day is today?");
+//        //reading of text string
+//        day = input.nextLine();
+//        System.out.println("Which month is today?");
+//        month = input.nextLine(); // nextLine only for string, for ex double is nextDouble
+//        String text;
+//        //displaying of data in console
+//        text = "Сегодня " + day + " " + "month " + month;
+//        System.out.println(text);
+//        int age, year, birth;
+//        String result;
+//        result = JOptionPane.showInputDialog("Which year is now?");
+//        //transformation of String text into int number
+//        year = Integer.parseInt(result);
+//        result = JOptionPane.showInputDialog("How old are you?");
+//        age = Integer.parseInt(result);
+//        birth = year - age;
+//        System.out.println("Today is: " + year + " " + "Your age is: " + age + " " + "You was borned in" + " " + birth + " " + "year");
+//        double mass, height, bmi;
+//        String result;
+//        result = JOptionPane.showInputDialog("Your weight in kilograms");
+//        mass = Double.parseDouble(result);
+//        result = JOptionPane.showInputDialog("Your heght in meters");
+//        height = Double.parseDouble(result);
+//        //counting of mass index
+//        bmi = mass/height/height;
+//        //rounding of the bmi result
+//        bmi = Math.round(bmi*100)/100.0;
+//        JOptionPane.showMessageDialog(null, "Index of your body mass is "+ bmi);
+//        System.out.printf("Your body mass index: %5.2f\n");
+    }
+}
